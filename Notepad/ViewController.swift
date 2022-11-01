b518970179b5ce6c4977c3091de2759caca48da3
Kostya Losev <Kostya777i@gmail.com>
2022-11-01 09:15:47 +0500

    Initial Commit

---
+++ Notepad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notepad
+//
+//  Created by Konstantin Losev on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
